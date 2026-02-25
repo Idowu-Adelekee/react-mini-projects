8dd34da0e8d92521309d5059490e8f51ec32ee6b
Idowu Adeleke <work2idowu@gmail.com>
2026-02-25 21:21:29 +0100

    Daily commitment

--- src/App.js
+++ src/App.js
@@ -25,35 +25,65 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(1);
+
   return (
     <div className="accordion">
       {data.map((item, i) => (
-        <Item title={item.title} num={i} text={item.text} key={item.title} />
+        <Item
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={item.title}
+          num={i}
+          text={item.text}
+          key={item.title}
+        />
       ))}
     </div>
   );
 }
 
-function Item({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
-  }
-
-  return (
-    <div className={isOpen ? `item open` : "item"} onClick={handleToggle}>
-      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
-
-      {isOpen ? (
-        <div className="content-box">
-          <p className="text">{text}</p>
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
+function Item({}) {
+  return <div></div>;
 }
+
+//   return (
+//     <div className="accordion">
+//       {data.map((item, i) => (
+//         <Item
+//           curOpen={curOpen}
+//           onOpen={setCurOpen}
+//           title={item.title}
+//           num={i}
+//           text={item.text}
+//           key={item.title}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function Item({ num, title, text, curOpen, onOpen }) {
+//   const isOpen = num === curOpen;
+
+//   function handleToggle() {
+//     // setIsOpen((isOpen) => !isOpen);
+//     onOpen(num);
+//   }
+
+//   return (
+//     <div className={isOpen ? `item open` : "item"} onClick={handleToggle}>
+//       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+//       <p className="title">{title}</p>
+//       <p className="icon">{isOpen ? "-" : "+"}</p>
+
+//       {isOpen ? (
+//         <div className="content-box">
+//           <p className="text">{text}</p>
+//         </div>
+//       ) : (
+//         ""
+//       )}
+//     </div>
+//   );
+// }
