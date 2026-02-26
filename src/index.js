5724c09e3b93b04f294e5cbc8a11382d9fba1eb4
Idowu Adeleke <work2idowu@gmail.com>
2026-02-26 14:05:49 +0100

    Daily commitment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./css/accordion.css";
 import "./css/pizzamenu.css";
+// import "./css/accordion.css";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
