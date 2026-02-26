80a2538221e796111c6d5153653955d990bb7075
Idowu Adeleke <work2idowu@gmail.com>
2026-02-26 22:08:40 +0100

    Basic clean up

--- src/components/PizzaMenu.js
+++ src/components/PizzaMenu.js
@@ -3,9 +3,3 @@ import { useState } from "react";
 export default function PizzaMenu() {
   return <div>Hello world</div>;
 }
-
-const idowu = "Noble";
-const idowu = "Noble";
-const idowu = "Noble";
-const idowu = "Noble";
-const idowu = "Noble";
